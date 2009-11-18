9c60669eb6943855ebbd4ed061d4d680f3c682bd
babilium <babilium@00390b90-d2d7-11de-a205-5990a31c5447>
2009-11-18 17:10:25 +0000

    Bon le temps c'est bien mais les fenêtres c'est encore mieu: SFML marche très bien, facile à installer, la grande classe.

--- trunk/ hululu/Test/src/Test.cpp
+++ trunk/ hululu/Test/src/Test.cpp
@@ -7,11 +7,25 @@
 //============================================================================
 
 #include <iostream>
+#include <SFML/System.hpp>
 using namespace std;
 
 int main()
 {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "Ca marche plutot bien mais je comprends pas encore tout.";
+
+	cout << "Il est temps de tester la SFML !" << endl;
+
+	/*
+    sf::Clock Clock;
+    while (Clock.GetElapsedTime() < 5.f)
+    {
+        std::cout << Clock.GetElapsedTime() << std::endl;
+        sf::Sleep(0.5f);
+    }
+    */
+
+
 	return 0;
 }
