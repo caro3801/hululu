e5bd8af5f897c3acbd408ac8e4304a2131659e00
babilium <babilium@00390b90-d2d7-11de-a205-5990a31c5447>
2009-11-18 18:00:29 +0000

    Hey on gère les évènement, cay cool !

--- trunk/ hululu/Test/src/Test.cpp
+++ trunk/ hululu/Test/src/Test.cpp
@@ -16,9 +16,14 @@ int main()
 {
 	cout << "Je vais ouvrir une fenetre !" << endl;
 
-	// a partir de maintenant je n'utilise plus sf:: (cf using namespace)
+	// je n'utilise pas obligatoirement sf:: (cf using namespace)
+	// c'est juste pour plus de lisibilite
+	// la plupart des objets, class et methodes utilisees sont issus de SFML
+	// l'auteur de la Doc utilise des conventions de nomage... pas conventionee
 
-	Window App(VideoMode(800, 600, 32), "Je suis vraiment une fenetre sexy");
+	// creation de la fenetre de 800px par 600px, profondeur de 32 bits pour les pixels
+	// et enfin le titre
+	Window fenetre(VideoMode(800, 600, 32), "Je suis vraiment un TITRE sexy");
 
 	// pour pas que le programme ne se termine pas :)
 	// on attent, si Running passe a false, on passe la boucle et on termine
@@ -26,13 +31,42 @@ int main()
 	// cette methode s'appelle le *polling* (le fait d'attendre ds une boucle)
 	// nous on a deja utilise des *callback*
 	// rappellez vous les fameux headers !
-	bool Running = true;
-	while (Running)
+
+	// GetEvent() c'est une pile d'evenements
+	// a chaque fois qu'un evenement survient
+	// GetEvent le chope et le fout dans Event
+	// Apres il suffit de lire le type de l'evenement
+	// ..
+	// "Un type d'evenement ? Mais Pierrot, la fievre te monte a la tete ?!"
+	// ne me regardez pas comme ca, vous allez comprendre...
+
+	bool running = true;
+	while(running)
 	{
-		// met juste a jour l'afficahge de la fenetre
-	    App.Display();
+		Event event;
+		while (fenetre.GetEvent(event))
+		{
+			// je laisse l'espace de nomage sf pour que vous voyez bien
+
+			// regardez comme c'est bien fait
+			// Event c'est une variable du type sf::Event
+			// elle a des valeurs contraintes
+			// quand elle vaut sf::Event::Closed
+			// c'est qu'on a ferme la fenetre !
+			if (event.Type == sf::Event::Closed)
+				running = false;
+
+			// pour la gestion des boutons, c'est un peu pareil
+			// si un bouton est presse et que c'est echap, alors bingo
+			if ((event.Type == sf::Event::KeyPressed) && (event.Key.Code == sf::Key::Escape))
+				running = false;
+		}
+
+		// toujour pour actualiser le rendu (et en fin de boucle surtout) !
+		fenetre.Display();
 	}
 
+	cout << "Hop hop, la boucle c'est fini !" << endl;
 
 
 	return 0;
