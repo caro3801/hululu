a818fa65617fd9021a1ef70afa5948f9b4d4905b
sof.richard <sof.richard@00390b90-d2d7-11de-a205-5990a31c5447>
2010-01-22 18:37:33 +0000

    Drag and Drop des objets : OK! -> Création d'une classe BoutonMove (utilisation tres simple :), exemple dans TestDeplacement). Intégration Perou OP, vous pouvez continuer le jeu ^^

---
+++ trunk/ hululu/le_voyage_de_barbulle/src/BoutonMove.cpp
@@ -0,0 +1,39 @@
+/*
+ * BoutonMove.cpp
+ *
+ *  Created on: 22 janv. 2010
+ *      Author: sophie
+ */
+
+#include "BoutonMove.h"
+
+BoutonMove::BoutonMove() : Bouton() {
+	this->decalageY=0;
+	this->decalageX=0;
+}
+
+BoutonMove::~BoutonMove() {
+}
+
+bool BoutonMove::enDeplacement(sf::RenderWindow &fenetre) {
+	//a modifier si jamais on veut que ce soit un autre bouton que clicGauche
+	if ( estClique(&fenetre) || sourisGaucheAppuye(fenetre) )
+		return true;
+	else
+		return false;
+}
+
+void BoutonMove::decalageSouris(float x, float y) {
+	this->decalageX=x;
+	this->decalageY=y;
+}
+
+void BoutonMove::deplacer(sf::RenderWindow &fenetre, bool mouseMove) {
+	if(enDeplacement(fenetre) && mouseMove) {
+		float posX = sourisPositionX(fenetre) - decalageX;
+		float posY = sourisPositionY(fenetre) - decalageY;
+		this->placer(posX, posY);
+	}
+}
+
+
---
+++ trunk/ hululu/le_voyage_de_barbulle/src/BoutonMove.h
@@ -0,0 +1,48 @@
+/*
+ * BoutonMove.h
+ *
+ *  Created on: 22 janv. 2010
+ *      Author: sophie
+ *
+ *  Classe héritée de Bouton permettant les memes fonctionnalités que celle-ci (objetCliquable)
+ *  et également de le déplacer (= Drag & Drop)
+ */
+
+
+#include "Bouton.h"
+
+#ifndef BOUTONMOVE_H_
+#define BOUTONMOVE_H_
+
+class BoutonMove : public Bouton {
+public:
+	BoutonMove();
+	virtual ~BoutonMove();
+
+	inline bool sourisDroitAppuye(sf::RenderWindow &fenetre) { return fenetre.GetInput().IsMouseButtonDown(sf::Mouse::Right); }
+	//renvoie vrai si le bouton droit de la souris est appuyé, faux sinon
+	inline bool sourisGaucheAppuye(sf::RenderWindow & fenetre) { return fenetre.GetInput().IsMouseButtonDown(sf::Mouse::Left); }
+	//renvoie vrai si le bouton gauche de la souris est appuyé, faux sinon
+
+	inline float sourisPositionX(sf::RenderWindow &fenetre) { return fenetre.GetInput().GetMouseX(); }
+	//renvoie la position de la souris en X dans la fenetre spécifiée
+	inline float sourisPositionY(sf::RenderWindow &fenetre) { return fenetre.GetInput().GetMouseY(); }
+	//renvoie la position de la souris en Y dans la fenetre spécifiée
+
+	bool enDeplacement(sf::RenderWindow &fenetre);
+	//savoir si l'objet est en train d'etre déplacé-> retour: - vrai si l'objet est cliqué OU le bouton de la souris appuyé,
+	//														  - faux sinon
+	void decalageSouris(float x, float y);
+	//Positionner l'objet par rapport a la souris pendant le déplacement: param: la valeur en X et Y du décalage souhaité
+	// (sinon par défaut l'objet aura son coin supérieur gauche a la place de la souris)
+
+	void deplacer(sf::RenderWindow &fenetre, bool mouseMove);
+	//déplacer l'objet a ses nouvelles coordonnées : déplacer l'objet si enDeplacement et la souris bouge dans la fenetre, ne rien faire sinon
+	//nouvelles coordonnées : sourisPosition(X,Y) + posSouris(X,Y) (pour le décalage)
+
+private:
+	float decalageY; //le décalage en Y de l'objet par rapport a la souris (0 par défaut)
+	float decalageX; //le décalage en X de l'objet par rapport a la souris (0 par défaut)
+};
+
+#endif /* BOUTONMOVE_H_ */
